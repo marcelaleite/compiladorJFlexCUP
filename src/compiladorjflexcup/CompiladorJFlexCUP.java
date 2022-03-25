7830548156ec1a914cf5fdcc1d2867fb10920cf1
Marcela <marcele.leite@gmail.com>
2022-03-25 08:23:58 -0300

    atualização

--- src/compiladorjflexcup/CompiladorJFlexCUP.java
+++ src/compiladorjflexcup/CompiladorJFlexCUP.java
@@ -5,7 +5,14 @@
  */
 package compiladorjflexcup;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.Scanner;
+import java_cup.runtime.Symbol;
 
 /**
  *
@@ -16,10 +23,42 @@ public class CompiladorJFlexCUP {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        String arquivo = "def_lexico.flex";
+    public static void main(String[] args) throws FileNotFoundException {
+        /*
+        geração do léxico
+        
+        String arquivo = "/home/marcela/NetBeansProjects/compiladorJFlexCUP/src/arquivos/lex.flex";
         File arq = new File(arquivo);
-        jflex.Main.generate(arq);
-    }
+        jflex.Main.generate(arq);*/
     
+    
+        /*
+        para geração do parser
+        java -jar java-cup-11b.jar -interface -parser Parser def.cup
+        */
+    
+        // para testar o léxico
+/*
+        try{
+               Scanner tec = new Scanner(System.in);
+               String s = tec.next();
+               while (!s.equals("s")){
+                    Reader entrada = new StringReader(s);
+                    Lexico lexico = new Lexico(entrada);
+                    System.out.println("Token: "+sym.terminalNames[lexico.next_token().sym]);
+                    s = tec.next();
+
+              }
+           }catch(Exception e){
+               System.out.println("Erro na entrada");
+           }    
+*/    
+        try{
+            FileReader arquivo = new FileReader(args[1]);
+            Lexico lex = new Lexico(arquivo);
+                    
+            Parser p = new Parser(lex);
+            p.parse();   
+            System.out.println("Texto Correto");
+        }catch(E)
 }
